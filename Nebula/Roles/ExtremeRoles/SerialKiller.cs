66ea6a8c627e6b2904cf5238f2a7641323d9f831
ZsFabTest <a1234567890001919@outlook.com>
2023-07-07 09:29:54 +0800

    230707 update

---
+++ Nebula/Roles/ExtremeRoles/SerialKiller.cs
@@ -0,0 +1,120 @@
+﻿namespace Nebula.Roles.ImpostorRoles;
+
+public class SerialKiller : Role
+{
+    public static Module.CustomOption killCooldownOption;
+    public static Module.CustomOption suicideMaxTimeOption;
+
+    private static CustomButton killButton;
+    public static CustomButton suicideButton;
+
+    private bool hasKilled = false;
+
+    private SpriteLoader SuicideButtonSprite = new SpriteLoader("Nebula.Resources.SuicideButton.png", 115f);
+
+    public override void LoadOptionData()
+    {
+        killCooldownOption = CreateOption(Color.white, "killCooldown", 20f, 10f, 30f, 2.5f);
+        suicideMaxTimeOption = CreateOption(Color.white, "suicideMaxTime", 40f, 20f, 60f, 5f);
+    }
+
+    public override void GlobalInitialize(PlayerControl __instance){
+        hasKilled = false;
+    }
+
+    public override void ButtonInitialize(HudManager __instance)
+    {
+        if (killButton != null)
+        {
+            killButton.Destroy();
+        }
+        killButton = new CustomButton(
+            () =>
+            {
+                PlayerControl target = Game.GameData.data.myData.currentTarget;
+                var res = Helpers.checkMuderAttemptAndKill(PlayerControl.LocalPlayer, target, Game.PlayerData.PlayerStatus.Dead, false, true);
+                killButton.Timer = killButton.MaxTimer;
+                Game.GameData.data.myData.currentTarget = null;
+                hasKilled = true;
+                suicideButton.Timer = suicideButton.MaxTimer;
+            },
+            () => { return !PlayerControl.LocalPlayer.Data.IsDead; },
+            () => { return PlayerControl.LocalPlayer.CanMove && Game.GameData.data.myData.currentTarget != null; },
+            () => { killButton.Timer = killButton.MaxTimer; },
+            __instance.KillButton.graphic.sprite,
+            Expansion.GridArrangeExpansion.GridArrangeParameter.AlternativeKillButtonContent,
+            __instance,
+            Module.NebulaInputManager.modKillInput.keyCode,
+            "button.label.kill"
+        ).SetTimer(CustomOptionHolder.InitialKillCoolDownOption.getFloat());
+        killButton.MaxTimer = killCooldownOption.getFloat();
+        killButton.SetButtonCoolDownOption(true);
+
+        if (suicideButton != null)
+        {
+            suicideButton.Destroy();
+        }
+        suicideButton = new CustomButton(
+            () => { },
+            () => { return !PlayerControl.LocalPlayer.Data.IsDead && hasKilled; },
+            () => { return true; },
+            () => { 
+                if(hasKilled) suicideButton.Timer = suicideButton.MaxTimer;
+                else suicideButton.Timer = 114514f;
+            },
+            SuicideButtonSprite.GetSprite(),
+            Expansion.GridArrangeExpansion.GridArrangeParameter.None,
+            __instance,
+            Module.NebulaInputManager.noGameInput.keyCode,
+            "button.label.suicide"
+        ).SetTimer(114514f);
+        suicideButton.MaxTimer = suicideMaxTimeOption.getFloat();
+        suicideButton.timeInVent = true;
+    }
+
+    public override void EditCoolDown(CoolDownType type, float count)
+    {
+        killButton.Timer -= count;
+        killButton.actionButton.ShowButtonText("+" + count + "s");
+    }
+
+    public override void MyPlayerControlUpdate()
+    {
+        Game.MyPlayerData data = Game.GameData.data.myData;
+        data.currentTarget = Patches.PlayerControlPatch.SetMyTarget(true);
+        Patches.PlayerControlPatch.SetPlayerOutline(data.currentTarget, Palette.ImpostorRed);
+        if(suicideButton.Timer <= 0)
+        {
+            RPCEventInvoker.UncheckedMurderPlayer(PlayerControl.LocalPlayer.PlayerId, PlayerControl.LocalPlayer.PlayerId, Game.PlayerData.PlayerStatus.Suicide.Id,true);
+            suicideButton.Timer = 114514f;
+            hasKilled = false;
+        }
+    }
+
+    public override void CleanUp()
+    {
+        base.CleanUp();
+        if(killButton != null)
+        {
+            killButton.Destroy();
+            killButton = null;
+        }
+        if(suicideButton != null)
+        {
+            suicideButton.Destroy();
+            suicideButton = null;
+        }
+        hasKilled = false;
+    }
+
+    public SerialKiller()
+    : base("SerialKiller", "serialkiller", Palette.ImpostorRed, RoleCategory.Impostor, Side.Impostor, Side.Impostor,
+                 Impostor.impostorSideSet, Impostor.impostorSideSet, Impostor.impostorEndSet,
+                 true, VentPermission.CanUseUnlimittedVent, true, true, true)
+    {
+        killButton = null;
+        suicideButton = null;
+        HideKillButtonEvenImpostor = true;
+        hasKilled = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+# Nebula-R LTS版本
+本模组是 Nebula on the ship 的分支版本 目前只有中文
+
+模组不盈利，只做单纯兴趣爱好，bug修不修看时间
+
+部分贴图取自TheOtherRoles-GM-Haoming和SuperNewRoles
+
+原模组于1.0.2.7版本停止更新，该版本游戏版本停留在v2023.3.28版本保持bug修复和部分实现
+
+## 参考
+<details>
+
+1.Nebula on the ship - Dolly1016
+
+2.Town of Us-R - eDonnes124
+
+3.TheOtherRole GM Haoming - haoming37
+
+4.SuperNewRoles - ykundesu
+
+5.模组汉化借鉴 - 四个憨批汉化组
+
+</details>
+
+## 联系作者
+email:a1234567890001919@outlook.com
+
+discord:zjy090820#9247
+
+QQ:3419987538
+
+# 多语言样式声明
+<details>
+<summary> 展开 </summary>
+
+## SChinese
+这些模组不隶属于 Among Us 或 Innersloth LLC，其包含的内容也未得到 Innersloth LLC 的认可或以其他方式赞助。此处包含的部分材料是Innersloth LLC的财产。
+## TChinese
+這些模組不隸屬於 Among Us 或 Innersloth LLC，其包含的內容也未得到 Innersloth LLC 的認可或以其他方式贊助。此處包含的部分材料是Innersloth LLC的財產。
+## English
+This mod is not affiliated with Among Us or Innersloth LLC, and the content contained therein is not endorsed or otherwise sponsored by Innersloth LLC. Portions of the materials contained herein are property of Innersloth LLC.
+## French
+Ce mod n'est pas affilié à Among Us ou à Innersloth LLC, et le contenu qu'il contient n'est pas approuvé ou autrement parrainé par Innersloth LLC. Certaines parties des documents contenus dans le présent document sont la propriété d'Innersloth LLC.
+## Japanese
+これらのモジュールは、Among UsまたはInnersloth LLCと提携しておらず、Innersloth LLCが承認またはその他の形でスポンサーとなっているコンテンツは含まれていません。 ここに含まれる資料の一部は、Innersloth LLCの所有物です。
+## Russian
+Эти модули не связаны с Among Us или Innersloth LLC и не содержат контента, который был одобрен или иным образом спонсирован Innersloth LLC. Некоторые из содержащихся здесь материалов являются собственностью Innersloth LLC.
+
+</details>
